ab1e52fc6a5a068abdad5d0fdfa95444282fba60
jmh3360 <jmh3360@gmail.com>
2019-06-11 17:28:50 +0900

    end fileupload

--- src/main/java/org/zerock/controller/UploadController.java
+++ src/main/java/org/zerock/controller/UploadController.java
@@ -3,6 +3,9 @@ package org.zerock.controller;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.nio.file.Files;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -21,6 +24,7 @@ import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.zerock.domain.AttachFileDTO;
@@ -32,6 +36,7 @@ import net.coobird.thumbnailator.Thumbnailator;
 @Log4j
 public class UploadController {
 	
+//	오늘 날짜 기준으로 폴더를 생성해준다 ex 2019/06/11
 	private String getFolder() {
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
@@ -42,7 +47,7 @@ public class UploadController {
 		
 		return str.replace("-", File.separator);
 	}
-	
+//	파일 형채를 체크
 	private boolean checkImageType(File file) {
 		
 		try {
@@ -94,6 +99,7 @@ public class UploadController {
 		
 		String uploadFolderPath = getFolder();
 		// make folder -----------------------
+		// 파라미터를 두개 전달한 이유는 앞에 것은 부모 디렉토리 그다음 것은 자식 디렉토리이다.
 		File uploadPath = new File(uploadFolder, uploadFolderPath);
 		log.info("upload path : " + uploadPath);
 		
@@ -108,23 +114,27 @@ public class UploadController {
 			AttachFileDTO attachDTO = new AttachFileDTO();
 			
 			String uploadFileName = multipartFile.getOriginalFilename();
-			
+			//IE 버젼때문에 아래 작업을 수행해야 함
 			uploadFileName = uploadFileName.substring(uploadFileName.indexOf("\\") + 1);
 			log.info("only file  name : "+ uploadFileName);
 			attachDTO.setFileName(uploadFileName);
+			//유니크한 아이디 생성
 			UUID uuid = UUID.randomUUID();
-			
+			//파일이름과 붙인다.
 			uploadFileName = uuid.toString() + "_" + uploadFileName;
 			
 			/* File saveFile = new File(uploadFolder,uploadFileName); */
 			
 			try {
+				//올린 파일과  동일한이름에 파일을 하나 만든다
 				File saveFile = new File(uploadPath,uploadFileName);
+				//잘 모름
 				multipartFile.transferTo(saveFile);
 				
 				attachDTO.setUuid(uuid.toString());
 				attachDTO.setUploadPath(uploadFolderPath);
 				
+				//올린 파일이  이미지 일 경우 화면에 보여줄 썸네일을 생성한다.
 				if (checkImageType(saveFile)) {
 					attachDTO.setImage(true);
 					FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath,"s_" +uploadFileName));
@@ -162,11 +172,60 @@ public class UploadController {
 	}
 	@GetMapping(value="/download",produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
 	@ResponseBody
-	public ResponseEntity<Resource> downloadFile(String fileName){
+	public ResponseEntity<Resource> downloadFile(@RequestHeader("User-Agent")String userAgent, String fileName){
 		
 		log.info("download file : " + fileName);
 		Resource resource = new FileSystemResource("c:\\upload\\"+fileName);
+		if (resource.exists() ==false) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 		
-		return null;
+		log.info("resources : " +resource);
+		String resoureceName = resource.getFilename();
+//		remove  UUID
+		String resourceOriginalName = resoureceName.substring(resoureceName.indexOf("_")+1);
+		HttpHeaders headers = new HttpHeaders();
+		try {
+			String downloadName = null;
+			if (userAgent.contains("Trident")) {
+				log.info("IE Browser");
+				downloadName  = URLEncoder.encode(resourceOriginalName,"UTF-8").replaceAll("\\\\", " ");
+				
+			}else if(userAgent.contains("Edge")) {
+				log.info("Edge browser");
+				downloadName  = URLEncoder.encode(resourceOriginalName,"UTF-8");
+				log.info("Edge name :  " + downloadName);
+			}else {
+				log.info("Chrome brower");
+				downloadName = new String(resourceOriginalName.getBytes("UTF-8"),"ISO-8859-1");
+			}
+			headers.add("Content-disposition", "attachment; filename="+downloadName);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity<Resource>(resource,headers,HttpStatus.OK);
+	}
+	@PostMapping("/deleteFile")
+	@ResponseBody
+	public ResponseEntity<String> deleteFile(String fileName, String type){
+		
+		log.info("deleteFile : " + fileName);
+		
+		File file;
+		
+		try {
+			file = new File("c:\\upload\\"+URLDecoder.decode(fileName, "UTF-8"));
+			file.delete();
+			if (type.equals("image")) {
+				String largeFileName = file.getAbsolutePath().replace("s_"," ");
+				log.info("largeFile Name : "+largeFileName);
+				file = new File(largeFileName);
+				file.delete();
+			}
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>("delete",HttpStatus.OK);
 	}
 }
--- src/main/webapp/WEB-INF/views/uploadAjax.jsp
+++ src/main/webapp/WEB-INF/views/uploadAjax.jsp
@@ -17,9 +17,29 @@
 	.uploadResult ul li {
 		list-style: none;
 		padding: 10px;
+		align-content: center;
+		text-align: center;
 	}
 	.uploadResult ul li img{
-		width: 20px;
+		width: 100px;
+	}
+	.uploadResult ul li span{
+		color: white;
+	}
+	.bigPictureWrapper{
+		position:absolute;
+		display: none;
+		justify-content: center;
+		align-items: center;
+		top: 0%;
+		width: 100%;
+		height: 100%;
+		background-color: gray;
+		z-index: 100;
+		background: rgba(255,255,255,0.5);
+	}
+	.bigPicture img{
+		width: 600px;
 	}
 </style>
 <title>Insert title here</title>
@@ -32,34 +52,75 @@
 	<div class="uploadResult">
 		<ul></ul>
 	</div>
+	<div class="bigPictureWrapper">
+		<div class="bigPicture"></div>
+	</div>
 	<script
   src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous">
 	</script>
-		
+				
 <script>
+//원본 이미지 보여주기 원본 이미지를 보여줄 div 생성
+function showImage(fileCallPath){
+	//alert(fileCallPath);
+	$(".bigPictureWrapper").css("display","flex").show();
+	
+	$(".bigPicture").html("<img src='/display?fileName="+encodeURI(fileCallPath)+"' />").animate({width:'100%',height:'100%'}, 1000);
+}
 $(document).ready(function(){
 	var regex = new RegExp("(.*?)\.(exe|sh|zip|alz)$");
 	var maxSize = 5242880; //5MB
 	var cloneObj = $(".uploadDiv").clone();
 	var uploadResult = $(".uploadResult ul");
+	$(".bigPictureWrapper").on("click",function(e){
+		$(".bigPicture").animate({width:'0%',height:'0%'},1000);
+			$(this).hide();
+	});
 	
+	$(".uploadResult").on("click","span",function(e){
+		var targetFile = $(this).data("file");
+		var type =$(this).data("type");
+		console.log(targetFile);
+		
+		$.ajax({
+			url: '/deleteFile',
+			data:{fileName:targetFile, type:type},
+			dataType : 'text',
+			type:'POST',
+			success:function(result){
+				console.log(result);
+			}
+		});
+	});
+	
+	//클릭 시 다운로드 실행 
+	//src에 controller 주소를 넣어  주면 컨트롤러 호출 된다 신기방기
 	function showUploadedFile(uploadResultArr){
 		var str = "";
 		console.log(uploadResultArr);
 		$(uploadResultArr).each(function(i,obj){
 			if (!obj.image) {
-			str +=  "<li><img src='/resources/img/attach.png'>"+obj.fileName+"</li>"
+				var fileCallPath = encodeURIComponent(obj.uploadPath+"/"+obj.uuid+"_"+obj.fileName); 
+				var fileLink = fileCallPath.replace(new RegExp(/\\/g),"/");
+				
+				str +=  "<li><div><a href='/download?fileName="+fileCallPath+"'><img src='/resources/img/attach.png'>"+obj.fileName+"</a>"
+						+"<span data-file=\'"+fileCallPath+"'\ data-type='file'>x</span></div></li>"
 			} else {
 				/* str += "<li>"+obj.fileName+"</li>"; */
 				var fileCallPath = encodeURIComponent(obj.uploadPath + "/s_"+obj.uuid+"_"+obj.fileName);
 				console.log(fileCallPath);
-				str +=  "<li>	<img src='/display?fileName="+fileCallPath+"'>"+obj.fileName+"</li>"
+				var originPath = obj.uploadPath+"\\"+obj.uuid+"_"+obj.fileName;
+				originPath = originPath.replace(new RegExp(/\\/g),"/");
+				
+				/* str +=  "<li><a href='/download?fileName="+fileCallPath+"'><img src='/display?fileName="+fileCallPath+"'></a></li>" */
+				str += "<li><a href=\javascript:showImage(\'"+originPath+"\')\><img src='/display?fileName="+fileCallPath+"'></a>"+
+						"<span data-file=\'"+fileCallPath+"'\ data-type='image'>x</span> </li>";
 			}
 		});
 		console.log(str);
 		uploadResult.append(str);
 	}
-	
+	/* 파일 벨리데이션  체크 */
 	function checkExtension(fileName, fileSize){
 		if (fileSize >= maxSize) {
 			alert("파일사이즈 초과");
@@ -83,10 +144,8 @@ $(document).ready(function(){
 			if (!checkExtension(files[i].name, files[i].size)) {
 				return false;
 			}
-			
 			formData.append("uploadFile",files[i]);
 		}
-		
 		$.ajax({
 			url:'/uploadAjaxAction',
 			processData:false,
@@ -98,7 +157,6 @@ $(document).ready(function(){
 				$(".uploadDiv").html(cloneObj.html());
 			}
 		});
-		
 	});
 });
 </script>
